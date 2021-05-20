60f6a5410d585bb6c0d7b519c18a02c818f98a40
roy-kisholoy <kisholoy@yeasitech.com>
2021-05-20 16:33:08 +0530

    Updated GIT IGNORE and build

---
+++ build/precache-manifest.30c8494a1ec5cd1acd69c95ca73d2791.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723fbd9c0f5893835f6e611512802a63",
+    "url": "/studentPortal/index.html"
+  },
+  {
+    "revision": "a381b2a31b794e67446f",
+    "url": "/studentPortal/static/css/3.861a8a52.chunk.css"
+  },
+  {
+    "revision": "e0ff00f4d51677b6e385",
+    "url": "/studentPortal/static/css/5.85f19f90.chunk.css"
+  },
+  {
+    "revision": "5b64f911d3196e4a0a9d",
+    "url": "/studentPortal/static/css/main.71407942.chunk.css"
+  },
+  {
+    "revision": "43ba6931fc792acd07ac",
+    "url": "/studentPortal/static/js/0.662ef15b.chunk.js"
+  },
+  {
+    "revision": "99ff3daf6bc077572ac9",
+    "url": "/studentPortal/static/js/10.906e6ed4.chunk.js"
+  },
+  {
+    "revision": "3ce1c9ec94f2a133377b",
+    "url": "/studentPortal/static/js/11.77850bee.chunk.js"
+  },
+  {
+    "revision": "d8159d42ddb0ad28a52f",
+    "url": "/studentPortal/static/js/12.bf352715.chunk.js"
+  },
+  {
+    "revision": "5ecddec3d751fd2edbf1",
+    "url": "/studentPortal/static/js/13.87c2c944.chunk.js"
+  },
+  {
+    "revision": "a381b2a31b794e67446f",
+    "url": "/studentPortal/static/js/3.d28c1fc9.chunk.js"
+  },
+  {
+    "revision": "648e0cf19ec644f1454296373a65e32f",
+    "url": "/studentPortal/static/js/3.d28c1fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5985297d295d9bec59fe",
+    "url": "/studentPortal/static/js/4.3a8bf488.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/studentPortal/static/js/4.3a8bf488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ff00f4d51677b6e385",
+    "url": "/studentPortal/static/js/5.969011f1.chunk.js"
+  },
+  {
+    "revision": "48763b73d8c195527ebddd5857148be0",
+    "url": "/studentPortal/static/js/5.969011f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad7edbea0eef2bdcb82",
+    "url": "/studentPortal/static/js/6.00418e02.chunk.js"
+  },
+  {
+    "revision": "38e34c5d665237a82b89",
+    "url": "/studentPortal/static/js/7.e9b40008.chunk.js"
+  },
+  {
+    "revision": "8e7fa176b006150306288bd092a696c0",
+    "url": "/studentPortal/static/js/7.e9b40008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564f65e12b79ef141b18",
+    "url": "/studentPortal/static/js/8.317e8201.chunk.js"
+  },
+  {
+    "revision": "ffc61f4d2321a875a9f7",
+    "url": "/studentPortal/static/js/9.20ac1ef9.chunk.js"
+  },
+  {
+    "revision": "5b64f911d3196e4a0a9d",
+    "url": "/studentPortal/static/js/main.35cfa9db.chunk.js"
+  },
+  {
+    "revision": "468dc5a4fd6205f0e7c0",
+    "url": "/studentPortal/static/js/runtime-main.bc14776a.js"
+  },
+  {
+    "revision": "0087dce419a7376eb06c837f740d31eb",
+    "url": "/studentPortal/static/media/CoreUI-Icons-Linear-Free.0087dce4.woff"
+  },
+  {
+    "revision": "089ab3c11c572362d088083c561cfa55",
+    "url": "/studentPortal/static/media/CoreUI-Icons-Linear-Free.089ab3c1.eot"
+  },
+  {
+    "revision": "1bc3764815bb391a16771c42c6e460ec",
+    "url": "/studentPortal/static/media/CoreUI-Icons-Linear-Free.1bc37648.ttf"
+  },
+  {
+    "revision": "4b57c6a5567c9ef09913983f82904faa",
+    "url": "/studentPortal/static/media/CoreUI-Icons-Linear-Free.4b57c6a5.svg"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/studentPortal/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2a103029dda6ab1bc9c8b0ee72536e41",
+    "url": "/studentPortal/static/media/Simple-Line-Icons.2a103029.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/studentPortal/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/studentPortal/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/studentPortal/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/studentPortal/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/studentPortal/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/studentPortal/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/studentPortal/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/studentPortal/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/studentPortal/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/studentPortal/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/studentPortal/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/studentPortal/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "e7f28f6a79a63b55649145913bb481f5",
+    "url": "/studentPortal/static/media/salesplash.e7f28f6a.png"
+  }
+]);
